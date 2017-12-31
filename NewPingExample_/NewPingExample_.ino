6183529d0b6c2ad2c5a202eb184d3196e426153f
JWKostoss <sharktooth5@sbcglobal.net>
2017-12-31 17:36:37 -0500

    tryAlternate
    Var 'count' will try and alternate checking inches and milliseconds.

--- NewPingExample_/NewPingExample_.ino
+++ NewPingExample_/NewPingExample_.ino
@@ -1,34 +1,52 @@
-// ---------------------------------------------------------------------------
-// Example NewPing library sketch that does a ping about 20 times per second.
-// ---------------------------------------------------------------------------
+// --------------------------------------------------------------
+// Example NewPing library sketch that does a ping about 20 Hz
+// --------------------------------------------------------------
 
 #include <NewPing.h>
 
-#define TRIGGER_PIN  9   // Arduino pin tied to trigger pin on the ultrasonic sensor.
-#define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
-#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in inches). Maximum sensor distance is rated at 157.48-196.85 in.
+#define TRIGGER_PIN  9   // trigger pin
+#define ECHO_PIN     11  // echo pin
+#define MAX_DISTANCE 200 // Maximum distance(in inches).
+//                          rated 157.48-196.85 in.
 
 int dist=0;
+int tiem=0;
+int count=0;
 
-void align(int val){
-  int ref=999;
+void align(int dig, int val){
+  int ref=9999;
+  switch(dig){
+    case 4:break;
+    case 3:ref/=10; break;
+    case 2:ref/=100; break;
+    case 1:ref/=1000; break;}
   while(ref>0){
     if(val>ref)break;
     else Serial.print(' '); ref/=10;}
   Serial.print(val);
 }
 
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance. AN INSTANCE
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+// NewPing setup of pins and maximum distance. AN INSTANCE
 
 void setup() {
-  Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
+  Serial.begin(115200); // Open serial monitor at 115200 baud
 }
 
 void loop() {
-  delay(50);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
-  //Serial.print("Ping: "); NO.
-  dist=sonar.ping_in(); // Send ping, get distance in inch
-  align(dist); // and print result (0 = outside set distance range) (but maybe just zero.)
-  if(dist==1)Serial.println(" inch");
-  else Serial.println(" inches");
+  delay(50);              // Wait 50ms between pings
+  //(about 20 pings/sec). 29ms be the shortest delay.
+  if((count%2)==0){
+    //Serial.print("Ping: "); NO.
+    dist=sonar.ping_in(); // Send ping, get distance in inch
+    align(3,dist); // and print result
+    //(0=outside set distance range) (but maybe just zero.)
+    if(dist==1)Serial.print(" inch");
+    else Serial.print(" inches");}
+  else if((count%2)==1){
+    tiem=sonar.ping();
+    align(4,tiem);
+    Serial.println("");}
+  else Serial.println("WAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+  count++;
 }
