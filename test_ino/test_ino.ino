50f46ff595325216f0e229e761b81ddd275bb1fd
JWKostoss <sharktooth5@sbcglobal.net>
2017-12-31 15:47:19 -0500

    onButton
    Leave 'trig' on while button.

--- test_ino/test_ino.ino
+++ test_ino/test_ino.ino
@@ -1,11 +1,13 @@
 void setup() {
   // put your setup code here, to run once:
   pinMode(10,INPUT);
+  pinMode(11,OUTPUT);
   pinMode(13,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(digitalRead(10)==HIGH)digitalWrite(13,1);
+  if(digitalRead(10)==HIGH){
+    digitalWrite(11,1); digitalWrite(13,1);}
   else digitalWrite(13,0);
 }
