ba5b877b09ce824bb9ee6e54c81a2e13f057cf29
JWKostoss <sharktooth5@sbcglobal.net>
2018-01-06 12:12:35 -0500

    adjust
    Hardware is common Cathode.

--- TricoLED/TricoLED.ino
+++ TricoLED/TricoLED.ino
@@ -7,15 +7,15 @@ void setup() {
 
 void loop() {
   int pot1 = analogRead(0);
-  int red = map(pot1, 0, 1023, 255, 0);
+  int red = map(pot1, 0, 1023, 0, 255);
 //  Serial.print(pot1); Serial.print(' ');
 //  Serial.print(red); Serial.print(' ');
   int pot2 = analogRead(1);
-  int grn = map(pot2, 0, 1023, 255, 0);
+  int grn = map(pot2, 0, 1023, 0, 255);
 //  Serial.print(pot2); Serial.print(' ');
 //  Serial.print(grn); Serial.print(' ');
   int pot3 = analogRead(2);
-  int blu = map(pot3, 0, 1023, 255, 0);
+  int blu = map(pot3, 0, 1023, 0, 255);
 //  Serial.print(pot3); Serial.print(' ');
 //  Serial.println(blu); Serial.println("");
   analogWrite(9, red);
