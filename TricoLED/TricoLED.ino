766330c0eec256730173816c6d85ce401c97b4ee
JWKostoss <sharktooth5@sbcglobal.net>
2018-01-06 11:53:05 -0500

    pushchanges
    you suck

--- TricoLED/TricoLED.ino
+++ TricoLED/TricoLED.ino
@@ -3,16 +3,26 @@ void setup() {
 pinMode(9, OUTPUT);
 pinMode(10, OUTPUT);
 pinMode(11, OUTPUT);
+Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 int pot1 = analogRead(0);
 int red = map(pot1, 0, 1023, 255, 0);
+Serial.print(pot1);
+Serial.print(' ');
+Serial.print(red);
 int pot2 = analogRead(1);
 int gren = map(pot2, 0, 1023, 255, 0);
+Serial.print(pot2);
+Serial.print(' ');
+Serial.print(gren);
 int pot3 = analogRead(2);
 int blu = map(pot3, 0, 1023, 255, 0);
+Serial.print(pot3);
+Serial.print(' ');
+Serial.print(blu);
 
 analogWrite(9, red);
 analogWrite(10, gren);
