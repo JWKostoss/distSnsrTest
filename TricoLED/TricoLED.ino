84c2b3bb15d82782465bec855764a6b7f4d0eec3
JWKostoss <sharktooth5@sbcglobal.net>
2018-01-06 12:24:44 -0500

    correction
    mundane light order change

--- TricoLED/TricoLED.ino
+++ TricoLED/TricoLED.ino
@@ -7,21 +7,21 @@ void setup() {
 
 void loop() {
   int pot1 = analogRead(0);
-  int grn = map(pot1, 0, 1023, 0, 255);
+  int blu = map(pot1, 0, 1023, 0, 255);
 //  Serial.print(pot1); Serial.print(' ');
 //  Serial.print(red); Serial.print(' ');
   int pot2 = analogRead(1);
-  int blu = map(pot2, 0, 1023, 0, 255);
+  int red = map(pot2, 0, 1023, 0, 255);
 //  Serial.print(pot2); Serial.print(' ');
 //  Serial.print(grn); Serial.print(' ');
   int pot3 = analogRead(2);
-  int red = map(pot3, 0, 1023, 0, 255);
+  int gren = map(pot3, 0, 1023, 0, 255);
 //  Serial.print(pot3); Serial.print(' ');
 //  Serial.println(blu); Serial.println("");
   if(red<=2)red=0;
-  if(grn<=2)grn=0;
+  if(gren<=2)gren=0;
   if(blu<=2)blu=0;
   analogWrite(9, red);
-  analogWrite(10, grn);
+  analogWrite(10, gren);
   analogWrite(11, blu);
 }
